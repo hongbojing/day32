050404f070501a72dd00a28fb623d2ccdcd2d5dd
Hongbo Jing <jack.jing7@gmail.com>
2015-07-28 01:43:28 -0400

    app.js comments update

--- builds/development/js/lib/angular/app.js
+++ builds/development/js/lib/angular/app.js
@@ -1,16 +1,21 @@
 angular.module('myApp',[])
 .controller("ColorController",function($scope,$timeout){
+
+  //name the function
   var updateColor=function(){
     $scope.arrayLength=Math.floor(Math.random()*100);
     $scope.colorArray=new Array($scope.arrayLength);
     for(var i=0;i<$scope.arrayLength;i++){
       $scope.colorArray[i]=Math.floor(Math.random()*100);
     }
+
+    //set the time cycle
     $timeout(function(){
       updateColor();
     },2000);
   };
 
+  //execute the function
   updateColor();
 })
 ;
